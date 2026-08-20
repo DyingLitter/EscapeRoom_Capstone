ac45072fbc7c4a2e2dde68f143b591d1d67cb584
DyingLitter <mahboihackaowl@gmail.com>
2026-08-20 18:36:05 +1000

    Revert "idk"
    This reverts commit e624995396790440f1d802a3638bfb3bb2fd2aa1.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,13 +4,11 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public bool StickGet;
-    public bool StickFixed;
     [SerializeField] Inventory Inventory;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         StickGet = false;
-        StickFixed = false;
     }
 
     // Update is called once per frame
